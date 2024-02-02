11369c40618664bcc9db8ea6d6b1c17a7fe75a48
clairehenrycreative <150551654+clairehenrycreative@users.noreply.github.com>
2024-02-01 22:00:48 -0600

    Add files via upload

---
+++ week3/tile/main.js
@@ -0,0 +1,47 @@
+function setup() {
+    createCanvas(200, 200);
+}
+
+function createTile() {
+    translate (0, 0);
+    fill('pink');
+    rect(0, 0, 200, 200);
+    stroke('orange');
+    strokeWeight(15);
+    line(0, 0, 200, 200);
+    line(200, 0, 0, 200);
+    noStroke();
+    fill('white');
+    circle(100, 50, 50);
+    circle(50, 100, 50);
+    circle(150, 100, 50);
+    circle(100, 150, 50);
+    noFill();
+    stroke('black');
+    strokeWeight('7');
+    circle(100, 50, 50);
+    circle(50, 100, 50);
+    circle(150, 100, 50);
+    circle(100, 150, 50);
+    noFill();
+    stroke('black');
+    strokeWeight('10');
+    circle (100, 100, 100);
+    noFill();
+    stroke('white');
+    strokeWeight('7');
+    circle (100, 100, 100);
+    noFill();
+    stroke('black');
+    strokeWeight('4');
+    circle (100, 100, 100);
+    circle (100, 100, 100);
+    noFill();
+    stroke('white');
+    strokeWeight('1');
+    circle (100, 100, 100);
+}
+
+function draw() {
+    createTile();
+}
